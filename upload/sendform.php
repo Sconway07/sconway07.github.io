93babcbb4441e3950c07354dd479db5a422699ff
Sconway07 <66034320+Sconway07@users.noreply.github.com>
2025-09-01 17:29:32 +0100

    Add files via upload

--- upload/sendform.php
+++ upload/sendform.php
@@ -18,14 +18,5 @@ if (isset($_POST['submit'])) {
     header( "Location: http://www.mettleofnettle.com/upload/index.html?mailsend" ) ;
 
 }
-    $name = $_REQUEST['name'] ;
-    $email = $_REQUEST['email'] ;
-    $phone = $_REQUEST['phone'] ;
-    $message = $_REQUEST['message'] ;
 
-    $content = "Name: $name\nEmail: $email\nPhone Number: $phone\nMessage: $message" ;
-
-    mail( "s.conway501@gmail.com", "Message from Nettle Contact Form", "Info:\n\n\$message", "From: $email") ;
-    
-    
 ?>
