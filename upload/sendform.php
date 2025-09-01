acf26bc2afabf539ceda6dd1204229cd0ab134fd
Sconway07 <66034320+Sconway07@users.noreply.github.com>
2025-09-01 17:25:56 +0100

    Add files via upload

--- upload/sendform.php
+++ upload/sendform.php
@@ -1,4 +1,23 @@
 <?php
+
+# submit is the name of the buttob
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'] ;
+    $subject = $_POST['subject'] ;
+    $mailFrom = $_POST['email'] ;
+    $message = $_POST['message'] ;
+
+
+    $mail_to = "s.conway501@gmail.com";
+    $headers = "From: ".$mailFrom ;
+    $txt = "Message from ".$name.".\n\n".$message;
+
+
+    # three parameters the email we will send to, the subject and the message
+    mail($mail_to, $subject, $txt, $headers);
+    header( "Location: http://www.mettleofnettle.com/upload/index.html?mailsend" ) ;
+
+}
     $name = $_REQUEST['name'] ;
     $email = $_REQUEST['email'] ;
     $phone = $_REQUEST['phone'] ;
@@ -8,6 +27,5 @@
 
     mail( "s.conway501@gmail.com", "Message from Nettle Contact Form", "Info:\n\n\$message", "From: $email") ;
     
-    header( "Location: http://www.mettleofnettle.com/upload/contact.html" ) ;
     
 ?>
